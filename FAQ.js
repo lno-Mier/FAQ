49d2da94a30bc6a925444e52326d4704ffd1d512
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-09-01 18:32:42 +0600

    Create FAQ.js

---
+++ FAQ.js
@@ -0,0 +1,18 @@
+function FAQ() {
+	let button1 = document.getElementById('button1').onclick = function() {
+		let answ1 = document.getElementById('answ1')
+		let visible1 = 'none'
+		answ1.style.display = visible1
+	}
+	let button2 = document.getElementById('button2').onclick = function() {
+		let answ2 = document.getElementById('answ2')
+		let visible2 = 'none'
+		answ2.style.display = visible2
+	}
+	let button3 = document.getElementById('button3').onclick = function() {
+		let answ3 = document.getElementById('answ3')
+		let visible3 = 'none'
+		answ3.style.display = visible3
+	}
+}
+FAQ()
