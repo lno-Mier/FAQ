df59b1a377bb8b44a0cfafef835d8b578668adcb
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-09-06 16:32:47 +0600

    Update FAQ.js

--- FAQ.js
+++ FAQ.js
@@ -1,13 +1,5 @@
 function FAQ() {
 	let visible = 'none'
-	let answ1 = document.getElementById('answ1').onclick = function() {
-		answ1.style.display = visible
-	}
-	let answ2 = document.getElementById('answ2').onclick = function() {
-		answ1.style.display = visible
-	}
-	let answ3 = document.getElementById('answ3').onclick = function() {
-		answ1.style.display = visible
-	}
-}
+	let answ = document.getElementById('answ').onclick = function() {
+		answ.style.display = visible
 FAQ()
