005dbb84ec5d2206e42e7835bcbf6b5e42b6ea2a
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-09-06 16:30:32 +0600

    Update FAQ.js

--- FAQ.js
+++ FAQ.js
@@ -3,12 +3,10 @@ function FAQ() {
 	let answ1 = document.getElementById('answ1').onclick = function() {
 		answ1.style.display = visible
 	}
-	let button2 = document.getElementById('button2').onclick = function() {
-		let answ2 = document.getElementById('answ2')
+	let answ2 = document.getElementById('answ2').onclick = function() {
 		answ1.style.display = visible
 	}
-	let button3 = document.getElementById('button3').onclick = function() {
-		let answ3 = document.getElementById('answ3')
+	let answ3 = document.getElementById('answ3').onclick = function() {
 		answ1.style.display = visible
 	}
 }
