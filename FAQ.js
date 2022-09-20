e1402af5da923f94550fb8e87817c712c7c8088e
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-09-20 17:11:10 +0600

    Update FAQ.js

--- FAQ.js
+++ FAQ.js
@@ -1,5 +1,14 @@
-function FAQ() {
-	let visible = 'none'
-	let answ = document.getElementById('answ').onclick = function() {
-		answ.style.display = visible
-FAQ()
+let list = document.querySelectorAll('.openbtn')
+console.log (list)
+for (let item of list) {
+	item.onclick = function() {
+		let parent = item.parentElement
+		let answ = parent.querySelector (".div_visible")
+		answ.style.display = 'block'
+		if (answ.style.display = 'block') {
+			item.onclick = function() {
+				answ.style.display = 'none'
+			}
+		}
+	}
+}
